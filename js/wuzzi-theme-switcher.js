759481dbfec8c226a08408ae0ce47f198a9d96f5
Kured <65931588+Kured@users.noreply.github.com>
2020-11-06 09:22:16 +0000

    Setup for darkmode system change

--- js/wuzzi-theme-switcher.js
+++ js/wuzzi-theme-switcher.js
@@ -30,97 +30,97 @@ window._toggleTheme = function(themeMode) {
        }     
    }
 
-   // toggle theme secondary (s)
-   $elementsThemeLs = document.querySelectorAll('.bg-lights');
-   $elementsThemeDs = document.querySelectorAll('.bg-darks');
+//    // toggle theme secondary (s)
+//    $elementsThemeLs = document.querySelectorAll('.bg-lights');
+//    $elementsThemeDs = document.querySelectorAll('.bg-darks');
 
-   // Light to Dark secondary
-   if ($elementsThemeLs) {
+//    // Light to Dark secondary
+//    if ($elementsThemeLs) {
        
-       for (let i=0; i<$elementsThemeLs.length; i++) {
-           let element = $elementsThemeLs[i];
+//        for (let i=0; i<$elementsThemeLs.length; i++) {
+//            let element = $elementsThemeLs[i];
 
-           element.classList.remove("bg-lights");
-           element.classList.add("bg-darks");
-       }     
-   }   
-   // Dark to Light secondary
-   if ($elementsThemeDs) {
-       for (let i=0; i<$elementsThemeDs.length; i++) {
-           let element = $elementsThemeDs[i];
-           element.classList.remove("bg-darks");
-           element.classList.add("bg-lights");
-       }     
-   }
+//            element.classList.remove("bg-lights");
+//            element.classList.add("bg-darks");
+//        }     
+//    }   
+//    // Dark to Light secondary
+//    if ($elementsThemeDs) {
+//        for (let i=0; i<$elementsThemeDs.length; i++) {
+//            let element = $elementsThemeDs[i];
+//            element.classList.remove("bg-darks");
+//            element.classList.add("bg-lights");
+//        }     
+//    }
    
-   // toggle theme text
-   $elementsThemeTextL = document.querySelectorAll('.text-light');
-   $elementsThemeTextD = document.querySelectorAll('.text-dark');
+//    // toggle theme text
+//    $elementsThemeTextL = document.querySelectorAll('.text-light');
+//    $elementsThemeTextD = document.querySelectorAll('.text-dark');
 
-   // Light to Dark
-   if ($elementsThemeTextL) {
+//    // Light to Dark
+//    if ($elementsThemeTextL) {
        
-       for (let i=0; i<$elementsThemeTextL.length; i++) {
-           let element = $elementsThemeTextL[i];
+//        for (let i=0; i<$elementsThemeTextL.length; i++) {
+//            let element = $elementsThemeTextL[i];
 
-           element.classList.remove("text-light");
-           element.classList.add("text-dark");
-       }     
-   }   
-   // Dark to Light
-   if ($elementsThemeTextD) {
-       for (let i=0; i<$elementsThemeTextD.length; i++) {
-           let element = $elementsThemeTextD[i];
-           element.classList.remove("text-dark");
-           element.classList.add("text-light");
-       }     
-   } 
+//            element.classList.remove("text-light");
+//            element.classList.add("text-dark");
+//        }     
+//    }   
+//    // Dark to Light
+//    if ($elementsThemeTextD) {
+//        for (let i=0; i<$elementsThemeTextD.length; i++) {
+//            let element = $elementsThemeTextD[i];
+//            element.classList.remove("text-dark");
+//            element.classList.add("text-light");
+//        }     
+//    } 
    
-   // toggle theme text secondary (s)
-   $elementsThemeTextLs = document.querySelectorAll('.text-lights');
-   $elementsThemeTextDs = document.querySelectorAll('.text-darks');
+//    // toggle theme text secondary (s)
+//    $elementsThemeTextLs = document.querySelectorAll('.text-lights');
+//    $elementsThemeTextDs = document.querySelectorAll('.text-darks');
 
-   // Light to Dark secondary
-   if ($elementsThemeTextLs) {
+//    // Light to Dark secondary
+//    if ($elementsThemeTextLs) {
        
-       for (let i=0; i<$elementsThemeTextLs.length; i++) {
-           let element = $elementsThemeTextLs[i];
+//        for (let i=0; i<$elementsThemeTextLs.length; i++) {
+//            let element = $elementsThemeTextLs[i];
 
-           element.classList.remove("text-lights");
-           element.classList.add("text-darks");
-       }     
-   }   
-   // Dark to Light secondary
-   if ($elementsThemeTextDs) {
-       for (let i=0; i<$elementsThemeTextDs.length; i++) {
-           let element = $elementsThemeTextDs[i];
-           element.classList.remove("text-darks");
-           element.classList.add("text-lights");
-       }     
-   }
+//            element.classList.remove("text-lights");
+//            element.classList.add("text-darks");
+//        }     
+//    }   
+//    // Dark to Light secondary
+//    if ($elementsThemeTextDs) {
+//        for (let i=0; i<$elementsThemeTextDs.length; i++) {
+//            let element = $elementsThemeTextDs[i];
+//            element.classList.remove("text-darks");
+//            element.classList.add("text-lights");
+//        }     
+//    }
     
-    // toggle theme text secondary (s)
-   $elementsThemeBtnLs = document.querySelectorAll('.btn-lights');
-   $elementsThemeBtnDs = document.querySelectorAll('.btn-darks');
+//     // toggle theme text secondary (s)
+//    $elementsThemeBtnLs = document.querySelectorAll('.btn-lights');
+//    $elementsThemeBtnDs = document.querySelectorAll('.btn-darks');
 
-   // Light to Dark secondary
-   if ($elementsThemeBtnLs) {
+//    // Light to Dark secondary
+//    if ($elementsThemeBtnLs) {
        
-       for (let i=0; i<$elementsThemeBtnLs.length; i++) {
-           let element = $elementsThemeBtnLs[i];
+//        for (let i=0; i<$elementsThemeBtnLs.length; i++) {
+//            let element = $elementsThemeBtnLs[i];
 
-           element.classList.remove("btn-lights");
-           element.classList.add("btn-darks");
-       }     
-   }   
-   // Dark to Light secondary
-   if ($elementsThemeBtnDs) {
-       for (let i=0; i<$elementsThemeBtnDs.length; i++) {
-           let element = $elementsThemeBtnDs[i];
-           element.classList.remove("btn-darks");
-           element.classList.add("btn-lights");
-       }     
-   }
+//            element.classList.remove("btn-lights");
+//            element.classList.add("btn-darks");
+//        }     
+//    }   
+//    // Dark to Light secondary
+//    if ($elementsThemeBtnDs) {
+//        for (let i=0; i<$elementsThemeBtnDs.length; i++) {
+//            let element = $elementsThemeBtnDs[i];
+//            element.classList.remove("btn-darks");
+//            element.classList.add("btn-lights");
+//        }     
+//    }
    // Styles update
    if ($elementThemeSetting.className === 'wz-light') {
        document.documentElement.style.setProperty('--main-ui', '#0A0C12');
